1e895af583b9133b27c36bee339f42ea3a377736
Ronik Mehra <ronikmehra8388@gmail.com>
2026-06-06 04:53:40 +0530

    moho has been added

---
+++ if_else.sh
@@ -0,0 +1,17 @@
+echo "Enter the name: "
+
+read name
+
+if [[ $name == "Ronik" || $name == "Abhishek" ]];then
+
+	echo "You are Smart"
+
+elif [ $name == "Annu" ];then
+	echo "You are Ghodu"
+
+else
+	echo "Nothing"
+
+fi
+
+
